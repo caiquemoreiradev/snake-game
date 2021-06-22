5a564714f48e4c7c25236151131e5faa1239ecf9
Caique Moreira <caiquemoreiradev@hmail.com>
2021-06-21 21:15:04 -0300

    movement snake

--- script.js
+++ script.js
@@ -2,9 +2,57 @@ let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 let box = 32;
 
-function criarBG() {
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+};
+
+let direction = "right";
+
+function createBG() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function createSnake() {
+    for (var i = 0; i < snake.length; i++) {
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+document.addEventListener('keydown', update);
+
+function update(e) {
+    if(e.keyCode == 37 && direction != "right") direction = "left";
+    if(e.keyCode == 38 && direction != "down") direction = "up";
+    if(e.keyCode == 39 && direction != "left") direction = "right";
+    if(e.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame() {
+
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
